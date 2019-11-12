d2f07400088fa6a6e9de9a8ffade042f9312a16d
Herb Torres <herb.torres@byhook.com>
2019-11-12 17:34:57 -0500

    adds some basic stuff

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,144 @@
 <style lang="scss">
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
   }
 
-  h1, h2 {
-    font-weight: normal;
+  body,
+  html {
+    height: 100%;
+    width: 100%;
+  }
+
+  body {
+    background: #ececec;
+    background-image: url('./assets/xmas2019-bg-main.jpg');
+    background-repeat: none;
+    background-size: cover;
+    background-position: center center;
+    color: #202124;
+    cursor: auto;
+    font-family: 'Open Sans', Arial, Helvetica, sans-serif;
+    font-size: 1em;
+    font-style: normal;
+    font-weight: 300;
+    line-height: 1.444;
+    margin: 0;
+    overflow-x: hidden;
+    padding: 0;
+    position: relative;
+    overflow-wrap: break-word;
+    word-wrap: break-word;
+  }
+
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  blockquote, q {
+    quotes: none;
+  }
+
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  p, h1, h2, h3,
+  h4, span, a {
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    text-rendering: optimizeLegibility;
+    color: #cf6059;
   }
 
   ul {
-    list-style-type: none;
+    list-style: none;
+    margin: 0;
     padding: 0;
   }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
+  p,
+  a,
+  span {
+    line-height: 1.71429;
+    font-size: 14px;
+    font-weight: 300;
+    letter-spacing: .25px;
+  }
+  h1,
+  h2 {
+    font-family: 'Mountains of Christmas',cursive;
+    font-size: 56px;
+    font-weight: 100;
   }
 
-  a {
-    color: #42b983;
+  h1 {
+    font-size: 32px;
+    font-weight: 400;
+    letter-spacing: 0;
+    line-height: 38px;
   }
+
+  h2 {
+    font-size: 28px;
+    font-weight: 500;
+    line-height: 1.25;
+  }
+
+  h3 {
+    font-family: 'Mountains of Christmas',cursive;
+    font-size: 20px;
+    font-weight: 300;
+    letter-spacing: 0;
+    line-height: 28px;
+  }
+
+  h4 {
+    font-size: 16px;
+    font-weight: 400;
+    letter-spacing: 0;
+    line-height: 24px;
+  }
+
+  /* 
+  red: #ef7561
+  green: #58bb97
+  dark: #57595b
+  offWhite: #ececec
+  */
 </style>
 
 <template>
   <div id="app">
+    <!-- <img src="./assets/xmas2019-bg-main.jpg"> -->
     <xmas_index></xmas_index>
     <!-- <img src="./assets/logo.png"> -->
     <!-- <h1>{{ msg }}</h1> -->
--- src/components/wheel.vue
+++ src/components/wheel.vue
@@ -1,50 +1,53 @@
 <style lang="scss" scoped>
-  .wheel {
 
+  .wheel__greeting {
+    display: block;
+    min-height: 400px;
+    text-align: center;
+    margin-top: 54px;
   }
 
-  #wheel,
-  #arrow {
+  .wheel__container {
+    position: relative;
+  }
+
+  .wheel__device,
+  .wheel__arrow {
     position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%)
+    transform: translate(-50%, -50%);
   }
 
   #arrow {
     cursor: pointer;
   }
 
-  #info {
-    position: fixed;
-    top: 0;
-    left: 0;
-    padding: 15px;
-    pointer-events: none;
-    font-size: 20px;
-    font-weight: bold;
-    font-family: sans-serif;
-  }
-
 </style>
 
 <template>
   <div class="wheel">
-    <svg id="wheel" width="458" height="458" version="1.1"
-        xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">
-      <image xlink:href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/wheel-1.png" x="0" y="0" height="458px" width="458px"/>
-    </svg>
+    <div class="wheel__greeting">
+      <h3>Torres Family wishes you</h3>
+      <h2 id="info"></h2>
+    </div>
 
-    <svg id="arrow" width="122" height="77" version="1.1"
-        xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">
-      <image xlink:href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/pin-1.png" x="0" y="0" height="77px" width="122px"/>
-    </svg>
+    <div class="wheel__container">
+      <svg class="wheel__device" id="wheel" width="458" height="458" version="1.1"
+          xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">
+        <image xlink:href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/wheel-1.png" x="0" y="0" height="458px" width="458px"/>
+      </svg>
 
-    <div id="info"></div>
+      <svg class="wheel__arrow" id="arrow" width="122" height="77" version="1.1"
+          xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">
+        <image xlink:href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/pin-1.png" x="0" y="0" height="77px" width="122px"/>
+      </svg>
+    </div>
   </div>
 </template>
 
 <script>
+  import $ from "jquery";
   import {TweenMax, TimelineMax} from 'gsap'
   import Draggable from 'gsap/Draggable'
 
@@ -63,46 +66,59 @@
       var numItems = 8;
       var arc = 360 / numItems;
       
-      TweenLite.set(wheel, { rotation: 360 });
+      TweenMax.set(wheel, { rotation: 360 });
       
-      var prizes = [
-        "Shirt",
-        "Free Bucket",
-        "Night Out #1",
-        "Je Vangt Bot",
-        "BioScoop Tickets",
-        "Night Out #2",
-        "Free Spin",
-          "HMH Tickets"
+      var greetings = [
+        'Merry Christmas',
+        'Happy Holidays',
+        'Yule Time Cheer',
+        'All the best',
+        'Best wishes',
+        'Warmest wishes',
+        'Christmas Greetings',
+        'Cheers to the New Year'
       ];
       
       arrow.on('click', () => {
-        
-          // var random = Math.floor(Math.random() * 8000) + 6000;
-          var random = Math.floor(Math.random() * 800) + 600;
-          var tl = new TimelineMax();
-          tl 
-            .to(wheel, 7, { rotation: "+=" + random, ease: Back.easeOut.config(1) })
 
-          TweenLite.to(tl, 4, { onComplete: () => {
-            console.log('fade me out maybe')
-          } })
+        var random = Math.floor(Math.random() * 800) + 600;
+        var tl = new TimelineMax();
+        tl 
+          .to(wheel, 7, { rotation: "+=" + random, ease: Back.easeOut.config(1) })
 
-          TweenLite.to(tl, 4, {timeScale:0, ease: Power1.easeOut, delay: 3, onComplete: () => {
+        TweenMax.to(tl, 3, { 
+          onComplete: () => {
+            console.log('hide result view now')
+            // TweenMax.to(info, 0.6, {
+            //   opacity: 0,
+            // })
+          }
+        })
+
+        TweenMax.to(tl, 4, {
+          timeScale:0, 
+          ease: Power1.easeOut, 
+          delay: 3, 
+          onComplete: () => {
             console.log('has ended')
-          } })
+            // TweenMax.to(info, 0.6, {
+            //   opacity: 1,
+            // })
+          } 
+        })
+
       })
       
-      TweenLite.ticker.addEventListener("tick", update);
+      TweenMax.ticker.addEventListener("tick", update);
       
       function update() {
         
         var rotation = wheel[0]._gsTransform.rotation;     
         var angle = rotation - 360 * Math.floor(rotation / 360);     
-        var index = Math.floor((360 - angle) / arc) % prizes.length;    
-        var prize = prizes[index];
+        var index = Math.floor((360 - angle) / arc) % greetings.length;    
+        var greeting = greetings[index];
         
-        info.html("Current Prize = " + prize);
+        info.html(greeting);
       }
     },
   }
